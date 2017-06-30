769e64a73599ff8ea1833d4646ac4ea52cad1d5a
Chase Moskal <lordbrimshaw@gmail.com>
2017-06-29 22:07:35 -0700

    Reorganize monarch test

--- s/Monarch.test.ts
+++ s/Monarch.test.ts
@@ -12,77 +12,70 @@ import {Entity, EntityRunInput, EntityRunOutput} from "./Entity"
 describe("Monarch", function() {
   this.slow(200)
 
-  it("runs initial entities every tick", async () => {
-    const {Ticker, report: tickerReport} = createSpyTickerClass()
-    const {Dog, report: dogReport} = createSpyDogClass()
-
-    const context = {host: true}
-    const network = new Network({context})
-    const simulator = new Simulator({context, entityClasses: {Dog}})
-
-    const monarch = new Monarch({
-      context, network, simulator, Ticker,
-      state: {A123: {type: "Dog"}}
-    })
-
-    monarch.start()
-    await sleep(35)
-
-    monarch.stop()
-    monarch.destructor()
-
-    expect(tickerReport.ticks).to.be.greaterThan(2)
-    expect(tickerReport.ticks).to.equal(dogReport.run)
-  })
-
   it("ticks between start and stop", async () => {
     const {Dog, report: dogReport} = createSpyDogClass()
     const {Ticker, report: tickerReport} = createSpyTickerClass()
-
     const context = {host: true}
     const network = new Network({context})
     const simulator = new Simulator({context, entityClasses: {Dog}})
+    const monarch = new Monarch({
+      context, network, simulator, Ticker,
+      state: {}
+    })
 
+    expect(tickerReport.ticks).to.equal(0)
+    monarch.start()
+
+    await sleep(21)
+    monarch.stop()
+    expect(tickerReport.ticks).to.be.greaterThan(1)
+    const ticks = clone(tickerReport.ticks)
+
+    await sleep(20)
+    expect(tickerReport.ticks).to.equal(ticks)
+    monarch.destructor()
+  })
+
+  it("runs initial entities every tick", async () => {
+    const {Ticker, report: tickerReport} = createSpyTickerClass()
+    const {Dog, report: dogReport} = createSpyDogClass()
+    const context = {host: true}
+    const network = new Network({context})
+    const simulator = new Simulator({context, entityClasses: {Dog}})
     const monarch = new Monarch({
       context, network, simulator, Ticker,
       state: {A123: {type: "Dog"}}
     })
 
-    expect(tickerReport.ticks).to.equal(0)
-
     monarch.start()
-    await sleep(35)
 
+    await sleep(21)
     monarch.stop()
-    expect(tickerReport.ticks).to.be.greaterThan(2)
-    const ticks = clone(tickerReport.ticks)
-    await sleep(20)
-  
-    expect(tickerReport.ticks).to.equal(ticks)
     monarch.destructor()
+
+    expect(tickerReport.ticks).to.be.greaterThan(1)
+    expect(tickerReport.ticks).to.equal(dogReport.run)
   })
 
   it("constructs, runs, and destructs dog entity", async () => {
     const {Ticker, report: tickerReport} = createSpyTickerClass()
     const {Dog, report: dogReport} = createSpyDogClass()
-
     const context = {host: true}
     const network = new Network({context})
     const simulator = new Simulator({context, entityClasses: {Dog}})
-
     const monarch = new Monarch({
       context, network, simulator, Ticker,
       state: {A123: {type: "Dog"}}
     })
 
     monarch.start()
-    await sleep(35)
 
+    await sleep(21)
     monarch.stop()
     monarch.destructor()
 
     expect(dogReport.constructed).to.be.equal(1)
-    expect(dogReport.run).to.be.greaterThan(2)
+    expect(dogReport.run).to.be.greaterThan(1)
     expect(dogReport.destructed).to.be.equal(1)
     expect(dogReport.woofs).to.equal(0)
   })
