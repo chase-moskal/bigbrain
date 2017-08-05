bdc8f32ba606a59041dfa62c7361e0910fc53a02
Chase Moskal <chasemoskal@gmail.com>
2017-08-04 21:26:28 -0700

    Tweak whitespace and import ordering

--- s/playground/Editor.ts
+++ s/playground/Editor.ts
@@ -2,16 +2,17 @@
 import {observable, computed, reaction, autorun} from "mobx"
 import {Scene, FreeCamera, Mesh, ShadowGenerator, SpotLight, Vector3} from "babylonjs"
 
-import Spectator, {makeCamera, applyLogicalMovement} from "./Spectator"
 import Ticker, {Tick} from "../Ticker"
 import {loadBabylonFile} from "../Susa"
 import {Entity, StateEntry, Message} from "../Monarch"
 import Watcher, {Input, Bindings, Status} from "../Watcher"
 
 import {PlaygroundContext} from "./Playground"
+import Spectator, {makeCamera, applyLogicalMovement} from "./Spectator"
 
 export default class Editor extends Entity {
   static readonly bindings: Bindings = {...Spectator.bindings}
+
   protected readonly context: PlaygroundContext
   private readonly camera: FreeCamera = makeCamera({scene: this.context.scene, position: this.entry.position})
   private readonly watcher = new Watcher({eventTarget: this.context.window, bindings: Editor.bindings})
@@ -21,6 +22,7 @@ export default class Editor extends Entity {
     ticker.start()
     return ticker
   })()
+
   destructor() {
     this.ticker.destructor()
   }
