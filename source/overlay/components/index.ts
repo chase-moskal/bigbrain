246362c016348e637f205480fbbf8a86536d13d6
Chase Moskal <chasemoskal@gmail.com>
2018-09-01 01:11:03 -0700

    update index.ts files

--- source/overlay/components/index.ts
+++ source/overlay/components/index.ts
@@ -1,4 +1,8 @@
 
 export * from "./components-interfaces"
+export * from "./context-menu"
+export * from "./main-menu"
+export * from "./menu-bar"
+export * from "./menu"
 export * from "./overlay"
 export * from "./stick"
--- source/overlay/stores/index.ts
+++ source/overlay/stores/index.ts
@@ -1,2 +1,8 @@
 
+export * from "./context-menu-store"
+export * from "./main-menu-store"
+export * from "./menu-bar-store"
+export * from "./menu-store"
+export * from "./overlay-store"
 export * from "./stick-store"
+export * from "./stores-interfaces"
