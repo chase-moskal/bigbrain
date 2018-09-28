6f940764efc51d5fb66a2ca41f971d694946d9dd
Chase Moskal <chasemoskal@gmail.com>
2018-09-27 20:52:53 -0700

    remove tank and simplify bundle

--- source/monarch-global.bundle.ts
+++ source/monarch-global.bundle.ts
@@ -1,9 +1,4 @@
 
 import * as monarch from "."
-import {initializeSlate} from "./slate/initialize-slate"
-import {initializePropshop} from "./propshop/initialize-propshop"
 
-const globals = {monarch, initializeSlate, initializePropshop}
-
-for (const key of Object.keys(globals))
-	window[key] = globals[key]
+window["monarch"] = monarch
